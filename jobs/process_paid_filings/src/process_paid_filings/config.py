b8597a7478267e95f31300fc3ea2cb998e499d9d
Chen <Steven.Chen@gov.bc.ca>
2024-10-03 09:42:39 -0700

    Duplicated AR entry created in COLIN by Business AR filing#328
    Signed-off-by: Chen <Steven.Chen@gov.bc.ca>

--- jobs/process_paid_filings/src/process_paid_filings/config.py
+++ jobs/process_paid_filings/src/process_paid_filings/config.py
@@ -57,18 +57,22 @@ class _Config(object):  # pylint: disable=too-few-public-methods
     COLIN_API_URL = os.getenv("COLIN_API_URL", "") + os.getenv("COLIN_API_VERSION", "")
     COLIN_API_SVC_CLIENT_ID = os.getenv("COLIN_API_SVC_CLIENT_ID")
     COLIN_API_SVC_CLIENT_SECRET = os.getenv("COLIN_API_SVC_CLIENT_SECRET")
-    
-    # POSTGRESQL
-    DATABASE_USERNAME = os.getenv('DATABASE_USERNAME', '')
-    DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', '')
-    DATABASE_NAME = os.getenv('DATABASE_NAME', '')
-    DATABASE_HOST = os.getenv('DATABASE_HOST', '')
-    DATABASE_PORT = os.getenv('DATABASE_PORT', '5432')
 
+    # POSTGRESQL
+    DB_USER = os.getenv("DATABASE_USERNAME", "")
+    DB_PASSWORD = os.getenv("DATABASE_PASSWORD", "")
+    DB_NAME = os.getenv("DATABASE_NAME", "")
+    DB_HOST = os.getenv("DATABASE_HOST", "")
+    DB_PORT = int(os.getenv("DATABASE_PORT", "5432"))  # POSTGRESQL
+    # POSTGRESQL
     if DB_UNIX_SOCKET := os.getenv("DATABASE_UNIX_SOCKET", None):
-        SQLALCHEMY_DATABASE_URI = f"postgresql+pg8000://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@/{DATABASE_NAME}?unix_sock={DB_UNIX_SOCKET}/.s.PGSQL.5432"
+        SQLALCHEMY_DATABASE_URI = (
+            f"postgresql+pg8000://{DB_USER}:{DB_PASSWORD}@/{DB_NAME}"
+            f"?unix_sock={DB_UNIX_SOCKET}/.s.PGSQL.5432"
+        )
     else:
-        SQLALCHEMY_DATABASE_URI = f"postgresql+pg8000://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+        SQLALCHEMY_DATABASE_URI = (
+            f"postgresql+pg8000://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
 
     TESTING = False
     DEBUG = False
