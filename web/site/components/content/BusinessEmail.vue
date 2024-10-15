d63bac4d0108385b29152f69fee56201418a3f44
EPortman <Ethan.Portman@gov.bc.ca>
2024-10-15 08:05:17 -0700

    fix: update business email to use newly updated prop name for contacts

--- web/site/components/content/BusinessEmail.vue
+++ web/site/components/content/BusinessEmail.vue
@@ -3,7 +3,7 @@ const busStore = useBusinessStore()
 const accountStore = useAccountStore()
 
 const displayEmail = computed(() => {
-  const mailingAddress = accountStore.currentAccount?.mailingAddress
+  const mailingAddress = accountStore.currentAccount?.contacts
   if (mailingAddress && mailingAddress.length > 0 && 'email' in mailingAddress[0]) {
     return mailingAddress[0].email
   } else {
